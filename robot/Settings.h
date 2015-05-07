9e961ec4d31eeaef9e6450c3ebbc72857070dfe7
Altex <d.ananyev@gmail.com>
2015-05-07 23:34:03 +0300

    wip

--- robot/Settings.h
+++ robot/Settings.h
@@ -19,11 +19,18 @@
 #define IR_SENSOR_4_PIN A1
 #define IR_SENSOR_5_PIN A7
 
+// Motor drivers pins
+// Left motor driver direction pin1
 #define LEFT_MOTOR_EN1_PIN 2
+// Left motor driver direction pin2
 #define LEFT_MOTOR_EN2_PIN 4
+// Left motor driver pwm pin
 #define LEFT_MOTOR_PWM_PIN 5
+// Right motor driver direction pin1
 #define RIGHT_MOTOR_EN1_PIN 7
+// Right motor driver direction pin2
 #define RIGHT_MOTOR_EN2_PIN 8
+// Right motor driver pwm pin
 #define RIGHT_MOTOR_PWM_PIN 6
 
 #endif
--- robot/Types.h
+++ robot/Types.h
@@ -17,18 +17,4 @@ enum WHEEL {
     WHEEL_COUNT
 };
 
-/**
- * Pins for each motor driver
- */
-enum WHEEL_PINS {
-    // Direction pin 1
-    WHEEL_PIN_EN1,
-    // Direction pin 2
-    WHEEL_PIN_EN2,
-    // Pwm pin
-    WHEEL_PIN_PWM,
-    // Total number of pins
-    WHEEL_PIN_COUNT
-};
-
 #endif
